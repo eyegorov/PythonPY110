4e61218d7ad427cf67fa4d7f751211ba57d71982
Evgenii Egorov <egorofevgeni@gmail.com>
2021-11-27 12:10:59 +0300

    Данное задание еще буду дорабатывать Буду благодарен за комментарии, так как далеко не все получилось

--- Занятие2/Домашние_задания/task3_2/main.py
+++ Занятие2/Домашние_задания/task3_2/main.py
@@ -1,3 +1,16 @@
+def positive_check(fn):
+    def wrapper(*args):
+        if args != int():
+            raise ValueError
+
+        result = fn(*args)
+        return result
+
+    return wrapper
+
+@positive_check
+def some_func(*args):
+    print(*args)
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    print(some_func(1,2,3)) # всё хорошо
+    print(some_func("r",5))  # должна быть вызвана ошибка ValueError
--- Занятие2/Домашние_задания/task3_3/main.py
+++ Занятие2/Домашние_задания/task3_3/main.py
@@ -1,3 +1,15 @@
+def iter_check(some_object):
+    def wrapper(*args, **kwargs):
+        if (hasattr(some_object, '__iter__') and hasattr(some_object. __next__) and some_object.__iter__() is some_object):
+            raise TypeError ("Объект <название или номер позиционного аргумента> <значение аргумента> не является итерируемым")
+        result = some_object(*args,**kwargs)
+        return result
+
 if __name__ == "__main__":
-    # Write your solution here
+    @iter_check
+    def some_func(*args, **kwargs):
+        print(*args, **kwargs)
+    if __name__ == "__main__":
+        print(some_func())
     pass
+
