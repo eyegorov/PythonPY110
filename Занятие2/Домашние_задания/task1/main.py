b05e4c29f0462ee29c6f94f78864a4c77783bd52
Evgenii Egorov <egorofevgeni@gmail.com>
2021-11-27 12:10:59 +0300

    init

--- Занятие2/Домашние_задания/task1/main.py
+++ Занятие2/Домашние_задания/task1/main.py
@@ -1,5 +1,7 @@
-
-
 if __name__ == "__main__":
-    # Write your solution here
+    def my_list(n, a0, a1):
+        for i in range(n):
+            yield a0 + i * a1
     pass
+
+     print(my_list(1,2,3))
